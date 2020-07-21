cd10de87fbf076d83b4a57262f5116ca1c059f5a
umesh kumawat <ukumawat@gmail.com>
2020-07-21 14:03:02 +0530

    Initial Commit

---
+++ APIKeyBasedAuthentication/Controllers/SecretController.cs
@@ -0,0 +1,20 @@
+﻿using APIKeyBasedAuthentication.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIKeyBasedAuthentication.Controllers
+{
+    [ApiKeyFilter]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet("secret")]
+        public IActionResult Secret()
+        {
+            return Ok("Hello from secret!");
+        }
+    }
+}
---
+++ APIKeyBasedAuthentication/Filters/ApiKeyFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIKeyBasedAuthentication.Filters
+{
+    public class ApiKeyFilterAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (context.HttpContext.Request.Headers.TryGetValue("ApiKey", out var apiKey))
+            {
+                var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+                var key = config.GetValue<string>("ApiKey");
+
+                if (!key.Equals(apiKey))
+                {
+                    context.Result = new UnauthorizedResult();
+                    return;
+                }
+            }
+            else
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+
+            await next();
+        }
+    }
+}
